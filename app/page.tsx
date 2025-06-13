539b301428cc062b2c93d6a7c32e02313180e45b
gary6668 <134781639+gary6668@users.noreply.github.com>
2025-06-13 18:05:05 +0800

    Normalize all line endings to LF

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Button size="lg" variant="default">
+      Click me
+    </Button>
+
+   
+  );
+}
